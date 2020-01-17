e1878f6fbb8e2ff4e3f2b203e9845b81694c3e04
Miguel Peralta <eudysperalta723@gmail.com>
2020-01-17 09:09:35 -0800

    ex1 complete

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -5,15 +5,31 @@ from hashtables import (HashTable,
                         hash_table_retrieve,
                         hash_table_resize)
 
+#input: weights = [ 4, 6, 10, 15, 16 ], length = 5, limit = 21
+#output: [ 3, 1 ]  # since these are the indices of weights 15 and 6 whose sum equals 21
 
+# Given a package with a weight limit `limit` and a list `weights` of item weights, implement a function `get_indices_of_item_weights` that finds two items whose sum of weights equals the weight limit `limit`. Your function will return an instance of an `Answer` tuple that has the following form:
+# ```
+# (zero, one)
+# ```
+# where each element represents the item weights of the two packages. _**The higher valued index should be placed in the `zeroth` index and the smaller index should be placed in the `first` index.**_ If such a pair doesn’t exist for the given inputs, your function should return `None`.
+
+# _NOTE:_ When calling `hash_table_retrieve` with a key that doesn't exist in the hash table, `hash_table_retrieve` will return `None`.
 def get_indices_of_item_weights(weights, length, limit):
     ht = HashTable(16)
-
-    """
-    YOUR CODE HERE
-    """
-
-    return None
+    for i in range(length):
+        hash_table_insert(ht, weights[i], i )
+    
+    for i in range(length):
+        s = hash_table_retrieve(ht, limit-weights[i])
+        if s is not None:
+            if s > i:
+                return(s,i)
+            else:
+                return(i, s)
+        
+            
+    return 
 
 
 def print_answer(answer):
